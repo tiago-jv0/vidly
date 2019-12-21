3e07ae88a80d9836b7a74b86cb44f582a700342f
Tiago José <tiago_jvo@outlook.com>
2019-12-21 17:51:24 -0300

    Extracting table body

--- src/components/MoviesTable.js
+++ src/components/MoviesTable.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Like } from './common/Like';
 import TableHeader from './common/TableHeader';
+import TableBody from './common/TableBody';
 
 class MoviesTable extends Component {
     constructor(props) {
@@ -12,37 +13,24 @@ class MoviesTable extends Component {
         { path: 'genre.name', label: 'Genre' },
         { path: 'numberInStock', label: 'Stock' },
         { path: 'dailyRentalRate', label: 'Rate' },
-        {key: 'like'},
-        {key : 'delete'},
+        { key: 'like', content: (movie) => <Like liked={movie.liked} onClick={() => this.props.onLike(movie)}></Like> },
+        {
+            key: 'delete',
+            content: (movie) => (
+                <button onClick={() => this.props.onDelete(movie)} className="btn btn-danger btn-sm">
+                    Delete
+                </button>
+            ),
+        },
     ];
 
     render() {
-        const { movies, sortColumn, onDelete, onLike, onSort } = this.props;
+        const { movies, sortColumn, onSort } = this.props;
         return (
             <div>
                 <table className="table">
                     <TableHeader columns={this.columns} sortColumn={sortColumn} onSort={onSort}></TableHeader>
-                    <tbody>
-                        {movies.map((movie) => {
-                            return (
-                                <tr key={movie._id}>
-                                    <td>{movie.title}</td>
-                                    <td>{movie.genre.name}</td>
-                                    <td>{movie.numberInStock}</td>
-                                    <td>{movie.dailyRentalRate}</td>
-                                    <td>
-                                        <Like liked={movie.liked} onClick={() => onLike(movie)}></Like>
-                                    </td>
-                                    <td>
-                                        <button onClick={() => onDelete(movie)} className="btn btn-danger btn-small">
-                                            {' '}
-                                            Delete
-                                        </button>
-                                    </td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
+                    <TableBody data={movies} columns={this.columns}></TableBody>
                 </table>
             </div>
         );
---
+++ src/components/common/TableBody.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import _ from 'lodash';
+
+export default class TableBody extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    renderCell = (item, column) => {
+        if (column.content) {
+             return column.content(item)
+        }
+
+        return _.get(item, column.path)
+    }
+
+    createKey = (item, column) => {
+        return item._id + (column.path || column.key)
+    }
+    render() {
+        const { data, columns } = this.props;
+
+        return (
+            <tbody>
+                {data.map((item) => {
+                    return (
+                        <tr key={item._id}>
+                            {columns.map((column) => {
+                                return <td key={this.createKey(item , column)}>{this.renderCell(item , column)}</td>;
+                            })}
+                        </tr>
+                    );
+                })}
+            </tbody>
+        );
+    }
+}
